5fe991ed69a1f56923a2507c84fcbfc1ade030bc
queryfear <queryfear@gmail.com>
2024-08-23 18:00:53 +0300

    Documentation #2

--- src/prog1main.cxx
+++ src/prog1main.cxx
@@ -6,14 +6,15 @@
 
 #define PORT 8080
 
-using namespace std;
-
 int main() {
     CreateData cd;
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
     socklen_t client_addr_len = sizeof(client_address);
 
+    // Создает новый сокет, AF_INET указывает на использование протокола IPv4
+    // SOCK_STREAM TCP
+    // Если создание сокета не удается, функция возвращает -1
     if ((server_socket = ::socket(AF_INET, 
                                 SOCK_STREAM,
                                  0)) == -1) {
@@ -23,8 +24,9 @@ int main() {
 
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = ::htons(PORT);
+    server_address.sin_port = ::htons(PORT); ///< Преобразует порт PORT в сетевой порядок байт   
 
+    // Привыязывает сокет к определенному адресу и порту
     if (::bind(server_socket, 
             (struct sockaddr*)&server_address, 
             sizeof(server_address)) == -1) {
@@ -33,6 +35,8 @@ int main() {
         return -1;
     }
 
+    // Устанавливает сервер в режим прослушивания входящих сообщений.
+    // 5 означает, что может обрботать до 5 входящих сообщений
     if (::listen(server_socket, 5) == -1) {
         std::cerr << "Failed to listen.\n";
         ::close(server_socket);
@@ -41,6 +45,7 @@ int main() {
 
     std::cout << "Server listening on port " << PORT << '\n';
 
+    // Блокирует выполнение программы до тех пор, пока не подключится клиент . Возвращает сокет для общения с клиентом
     if ((client_socket = ::accept(server_socket, 
                                 (struct sockaddr*)&client_address,
                                  &client_addr_len)) == -1) {
@@ -67,6 +72,6 @@ int main() {
     t1.join();
     t2.join();
 
-    close(client_socket);
-    close(server_socket);
+    ::close(client_socket);
+    ::close(server_socket);
 }
